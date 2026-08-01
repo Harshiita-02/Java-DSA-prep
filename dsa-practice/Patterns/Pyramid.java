6d2296f5e16bd23962c1f61f1a4fb49295dcca68
Harshita Shekhawat <h.shekhawat0248@gmail.com>
2026-08-01 11:10:26 +0530

    pyramid

--- dsa-practice/Patterns/Pyramid.java
+++ dsa-practice/Patterns/Pyramid.java
@@ -5,7 +5,7 @@ public class Pyramid {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         for(int i =0; i<n ; i++){
-            for(int j =0; j<n-i; j++){
+            for(int j =0; j<n-i-1; j++){
                 System.out.print(" ");}
             for(int j =0; j<2*i+1; j++){
                 System.out.print("*");}
