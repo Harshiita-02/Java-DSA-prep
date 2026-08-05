02f81a8b500fa64524c7340eed0db8f11e6adbdf
Harshita Shekhawat <h.shekhawat0248@gmail.com>
2026-08-05 22:27:35 +0530

    LL easy-medium

---
+++ dsa-practice/LinkedList/Middle_of_LL.java
@@ -0,0 +1,51 @@
+class Node{
+    int data;
+    Node next;
+    Node(int data){
+        this.data = data;
+        this.next = null;}
+
+    Node(int data, Node next){
+        this.data = data;
+        this.next = next;}}
+
+
+public class Middle_of_LL{
+
+    public static Node mid(Node head){// TC = O(N + N/2) = O(N), SC = O(1)
+        if(head == null){return null;}
+        Node temp = head;
+        int count = 0;
+        while(temp!=null){
+            count++;
+            temp=temp.next;}
+
+        int midNode = (count/2)+1;
+        temp = head;
+        while(temp!=null){
+            midNode = midNode-1;
+            if(midNode==0){break;}
+            temp=temp.next;}
+        return temp;}
+
+    public static Node fast_slow_midNode(Node head){//TC = O(N/2) = O(N), SC = O(1)
+        Node fast = head;
+        Node slow = head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;}
+        return slow;}
+
+    public static void main(String[] args){
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+        head.next.next.next.next = new Node(5);
+        head.next.next.next.next.next = new Node(6);
+
+        // Node midNode = mid(head);
+        Node midNode = fast_slow_midNode(head);
+        System.out.println("Middle node data: " + midNode.data);
+    }
+}
---
+++ dsa-practice/LinkedList/ReverseLL.java
@@ -0,0 +1,74 @@
+import java.util.Stack;
+class Node {
+    int data;
+    Node next;
+
+    public Node(int data){
+        this.data=data;
+        this.next= null;}
+
+    public Node(int data, Node next){
+        this.data=data;
+        this.next= next;}
+}
+
+public class ReverseLL{
+    public static void traversal(Node head){
+        Node temp = head;
+        while(temp!=null){
+            System.out.print(temp.data+" -> ");
+            temp=temp.next;
+        }
+        System.out.println("null");
+}
+    public static Node iterative1(Node head){
+        Node temp = head;
+        Stack<Integer> stack = new Stack<>();
+        while(temp!=null){
+            stack.push(temp.data);
+            temp = temp.next;}
+
+        temp = head;
+        while(temp!=null){
+            temp.data = stack.peek();
+            stack.pop();
+            temp = temp.next;
+        }
+        return head;
+    }
+
+    public static Node iterative2(Node head){
+        Node temp = head;
+        Node prev = null;
+        while(temp!=null){
+            Node front = temp.next;
+            temp.next=prev;
+            prev=temp;
+            temp=front;}
+        return prev;
+    }
+
+    public static Node recursive(Node head){
+        if(head == null || head.next == null){
+            return head;}
+            Node newHead=recursive(head.next);
+            Node front = head.next;
+                front.next = head;
+                head.next= null;
+            
+        return newHead;
+    }
+    public static void main(String[] args) {
+        Node head = new Node(4);
+        head.next=new Node(5);
+        head.next.next=new Node(1);
+        head.next.next.next=new Node(8);
+        head.next.next.next.next=new Node(2);
+        traversal(head);
+        // iterative1(head);
+        // Node newhead = iterative2(head);
+        Node newhead = recursive(head);
+        traversal(newhead);
+    }
+    
+}
