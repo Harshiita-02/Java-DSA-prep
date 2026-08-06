e62f8b62514a172aaa5fe533527559deb3931ab0
Harshita Shekhawat <h.shekhawat0248@gmail.com>
2026-08-06 16:14:23 +0530

    loop in LL basic problems

--- dsa-practice/LinkedList/LengthOfLoop.java
+++ dsa-practice/LinkedList/LengthOfLoop.java
@@ -10,27 +10,42 @@ class Node{
         this.next = next;}}
 
 
-public class DetectLoop {
-    public static Boolean loop1(Node head){//TC = O(2N*logN/1)   SC = O(N)
+public class LengthOfLoop {
+    public static int loop1(Node head){//TC = O(2N*logN/1)   SC = O(N)
         HashMap<Node,Integer> map = new HashMap<>();
         Node temp = head;
+        int timer=0;
         while (temp!=null) {
-            if(map.containsKey(temp)){return true;}
-                map.put(temp, 1);
-                temp = temp.next;
-        }
+            if(map.containsKey(temp)){
+                int looplength = timer-map.get(temp);
+                return looplength;}
+                map.put(temp, timer);
+                timer++;
+                temp = temp.next;}
+        return 0;}
 
-        return false;}
-
-    public static Boolean loop2(Node head){//TC = O(N)   SC = O(1)
+    public int loop2(Node head){//TC = O(N)   SC = O(1)
         Node slow = head;
         Node fast = head;
         while(fast!=null && fast.next!=null){
             slow=slow.next;
             fast=fast.next.next;
-            if(slow==fast){return true;}
+            if(slow==fast){
+                return countLoopLength(slow);
+            }
         }
-        return false; }
+        return 0; }
+
+        public int countLoopLength(Node meetingPoint) {
+        Node temp = meetingPoint;
+        int length = 1;
+        // Move until we meet again
+        while (temp.next != meetingPoint) {
+            temp = temp.next;
+            length++;
+        }
+        return length;
+    }
     public static void main(String[] args) {
         Node head = new Node(1);
         head.next = new Node(2);
@@ -41,11 +56,5 @@ public class DetectLoop {
         head.next.next.next.next.next.next = new Node(7);
         head.next.next.next.next.next.next.next = new Node(8);
         head.next.next.next.next.next.next.next.next = new Node(9,head.next.next.next);
-
-        if(loop1(head)){System.out.println("has a loop");}
-        else{System.out.println("no loop");}
-
-        if(loop2(head)){System.out.println("has a loop");}
-        else{System.out.println("no loop");}
     }
 }
--- dsa-practice/LinkedList/StartingPointOfLoop.java
+++ dsa-practice/LinkedList/StartingPointOfLoop.java
@@ -10,27 +10,34 @@ class Node{
         this.next = next;}}
 
 
-public class DetectLoop {
-    public static Boolean loop1(Node head){//TC = O(2N*logN/1)   SC = O(N)
+public class StartingPointOfLoop {
+    public static Node loop1(Node head){//TC = O(2N*logN/1)   SC = O(N)
         HashMap<Node,Integer> map = new HashMap<>();
         Node temp = head;
         while (temp!=null) {
-            if(map.containsKey(temp)){return true;}
+            if(map.containsKey(temp)){return temp;}
                 map.put(temp, 1);
                 temp = temp.next;
         }
 
-        return false;}
+        return null;}
 
-    public static Boolean loop2(Node head){//TC = O(N)   SC = O(1)
+    public static Node loop2(Node head){//TC = O(N)   SC = O(1)
         Node slow = head;
         Node fast = head;
         while(fast!=null && fast.next!=null){
             slow=slow.next;
             fast=fast.next.next;
-            if(slow==fast){return true;}
+            if(slow==fast){
+                slow = head;
+                while(slow!=fast){
+                    slow = slow.next;
+                    fast=fast.next;
+                }
+                return slow;
+            }
         }
-        return false; }
+        return null; }
     public static void main(String[] args) {
         Node head = new Node(1);
         head.next = new Node(2);
@@ -41,11 +48,5 @@ public class DetectLoop {
         head.next.next.next.next.next.next = new Node(7);
         head.next.next.next.next.next.next.next = new Node(8);
         head.next.next.next.next.next.next.next.next = new Node(9,head.next.next.next);
-
-        if(loop1(head)){System.out.println("has a loop");}
-        else{System.out.println("no loop");}
-
-        if(loop2(head)){System.out.println("has a loop");}
-        else{System.out.println("no loop");}
     }
 }
