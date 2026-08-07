329543d84642c31073c494d32c9ce8e57d59d36e
Harshita Shekhawat <h.shekhawat0248@gmail.com>
2026-08-07 21:56:56 +0530

    starting trees

---
+++ dsa-practice/Trees/TreeNode.java
@@ -0,0 +1,11 @@
+public class TreeNode {
+    int data;
+    Node left;
+    Node right;
+
+    public TreeNode(int data){
+        this.data=data;
+        left=null;
+        right=null;
+    }
+}
